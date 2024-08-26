67c3fb42b120c6c4fb6c4a373e3280b1ddd48451
Starboy <samskywalker001@gmail.com>
2024-08-26 20:32:16 +0530

    initials

--- TypeScript/main.test.ts
+++ TypeScript/main.test.ts
@@ -1,23 +1,50 @@
 import { expect, test } from "vitest";
 
-let array = ['hello', 'world', 'wtf', 'fml', 'idk'];
+let array: string[] = ["hello", "world", "wtf", "fml", "idk"];
 
-import { sortThis } from './src/binarySearch';
-test('Sorted Array', () => {
-    expect(sortThis(array)).toStrictEqual(['fml', 'hello', 'idk', 'world', 'wtf']);
-})
+import { sortThis } from "./src/binarySearch";
+test("Sorted Array", () => {
+  expect(sortThis(array)).toStrictEqual([
+    "fml",
+    "hello",
+    "idk",
+    "world",
+    "wtf",
+  ]);
+});
 
-import { binarySearch } from './src/binarySearch';
-test('Binary Search', () => {
-    expect(binarySearch(array, 'wtf')).toBe(2)
-})
+import { binarySearch } from "./src/binarySearch";
+test("Binary Search", () => {
+  expect(binarySearch(array, "wtf")).toBe(2);
+});
 
+let theArray: string[] = [
+  "Sam",
+  "Starboy",
+  "Tris",
+  "Stargirl",
+  "Life",
+  "Universe",
+  "&",
+  "Whatever",
+  "tf",
+  "lies",
+  "beyond...",
+  "wtf, right?",
+  "Origins",
+  "Eternity",
+  "The Beginning of the end",
+  "Singularity",
+  "FML",
+  "Fuck",
+  "Just",
+  "Fuckin'",
+  "Die",
+  "Already",
+  "...",
+];
 
-let theArray = ['Sam', 'Starboy', 'Tris', 'Stargirl', 'Life', 'Universe', '&', 'Whatever', 'tf', 'lies', 'beyond...', 'wtf, right?', 'Origins', 'Eternity', 'The Beginning of the end', 'Singularity', 'FML', 'Fuck', 'Just', 'Fuckin\'', 'Die', 'Already', '...']
-
-
-import { binarySearch } from './src/binarySearch';
-test('Another BS', () => {
-    expect(binarySearch(theArray, 'Universe')).toBe(5)
-})
-
+// import { binarySearch } from "./src/binarySearch";
+// test("Another BS", () => {
+//   expect(binarySearch(theArray, "Universe")).toBe(5);
+// });
